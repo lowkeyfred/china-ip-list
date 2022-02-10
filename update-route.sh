ab0d5c0f79d15293c7f43c93b0c611c88d34e6a4
lowkeyfred <lowkeyfred@gmail.com>
2022-02-11 02:32:05 +0800

    rm file before wget

--- update-route.sh
+++ update-route.sh
@@ -1,8 +1,11 @@
 #!/bin/ash
 # generate ip route based on china ip list
-wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
 
 FILE=china_ip_list.txt
+
+rm -f $FILE
+
+wget https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
 
 if [ -f "$FILE" ]; then
     while read ip; do
